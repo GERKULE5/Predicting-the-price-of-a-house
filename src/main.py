34a063ea7d61f937a9535181c89e9ddee3603977
obscure <konstantinkochengin44@gmail.com>
2025-09-16 01:44:35 +0700

    add repair_dict and refctor

--- src/main.py
+++ src/main.py
@@ -2,44 +2,93 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
-
-repairIndex = {
-    'Designed': 5,
-    'Euro': 4,
-    'Cosmetic': 3
-}
+import os
+from repair_dict import repairLevelIndex
 
 
-data = pd.read_csv('src/dataset.csv')
-print(f'Dataset:\n{data}')
 
-x = data.drop(['price'], axis=1)
-y = data['price']
+class Prediction: 
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=10, random_state=42)
+    def readData(self, path: str):
+        try:
+            data = pd.read_csv(path)
+            
+            print(f"Data: \n{data}")
+            return data
 
-model = LinearRegression()
+        except FileNotFoundError as e:
+            return (f'Error: {e}')
+
+        except Exception as e:
+            return (f'Critical error: Cant to read file. Please check the expansion of file must be .csv')
+        
+    def prepareData(self, data):
+        try:
+            x = data.drop(['price'], axis=1)
+            y = data['price']
+            return x, y
+
+        except Exception as e:
+            return e
+    
+    def trainModel(self, x, y):
+        try: 
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=10, random_state=42)
+            model = LinearRegression()
+            model.fit(x_train, y_train)
+            return model, x_test, y_test
+        
+        except Exception as e:
+            return e
+
+    def testingModel(self, model, x_test, y_test):
+        preds = model.predict(x_test)
+
+        print(f'Predictions: {preds}')
+        print(f'Correct answers: {y_test.values}')
+
+        mae = mean_absolute_error(y_test, preds)
+        print(f'Average error: {mae}')
 
 
-model.fit(x_train, y_train)
+        print(f'Coef: {model.coef_}')
+        print(f'Intercept: {model.intercept_}')
+    
+    def predicting(self, new_flat: list, model):
+        inputData = pd.DataFrame([[new_flat[0], new_flat[1], repairLevelIndex[new_flat[2]]]], columns=['s', 'roomsCount', 'Repair'])
+        predictedPrice = model.predict(inputData)
 
-preds = model.predict(x_test)
-
-print(f'Predictions: {preds}')
-print(f'Correct answers: {y_test.values}')
-
-mae = mean_absolute_error(y_test, preds)
-print(f'Average error: {mae}')
+        print(f'Predicted price: {predictedPrice[0]}')
 
 
-print(f'Coef: {model.coef_}')
-print(f'Intercept: {model.intercept_}')
+def main():
+    pred = Prediction()
 
-new_flat = pd.DataFrame([[35, 2, repairIndex['Euro']]], columns=['s', 'roomsCount', 'Repair'])
+    data = pred.readData('src/dataset.csv')
+    if data is not None:
+        x,y = pred.prepareData(data)
+        model, x_test, y_test = pred.trainModel(x,y)
+        pred.testingModel(model, x_test, y_test)
 
-predictedPrice = model.predict(new_flat)
+    new_flat = []
+    s = float(input('Area: '))
+    roomsCount = int(input('Count of rooms(If its studio = 1): '))
+    repairLevel = str(input('Repair level(Designed/Euro/Cosmetic): '))
 
-print(f'Predicted price: {predictedPrice[0]}')
+    if repairLevel.strip() not in ['Designed','Euro', 'Cosmetic']:
+        print('Unknown repair')
+        return
+
+    new_flat.append(s)
+    new_flat.append(roomsCount)
+    new_flat.append(repairLevel)
+
+    pred.predicting(new_flat, model)
+
+
+    
+if __name__ == "__main__":
+    main()
 
 
 
---
+++ src/repair_dict.py
@@ -0,0 +1,5 @@
+repairLevelIndex = {
+    'Designed': 5,
+    'Euro': 4,
+    'Cosmetic': 3
+}
